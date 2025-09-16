556b4f0da27aa383c0fd9a2259122950b81537f2
Erick Fanka <erickfanka@gmail.com>
2025-09-16 13:03:21 +0000

    Update respostas

--- facil/atividade4.py
+++ facil/atividade4.py
@@ -1,4 +1,2 @@
 # Atividade 4 – Tipos de dados básicos
 # Crie variáveis de tipos diferentes (int, float, string, bool) e exiba seus valores.
-
- 
---
+++ respostas/facil/atividade9.py
@@ -0,0 +1,11 @@
+# Solução Atividade 9
+nota = 85
+
+if nota >= 90:
+    print("Excelente")
+elif nota >= 80:
+    print("Muito bom")
+elif nota >= 70:
+    print("Bom")
+else:
+    print("Precisa melhorar")
